15976c64b1c2f7a78951354b061c47bd115bc7a7
letek <tomasz.letki@gmail.com>
2020-11-06 13:08:28 +0100

    Test 3 testy 2

--- src/main/java/pl/sda/javatarr6/todo/DataLoader.java
+++ src/main/java/pl/sda/javatarr6/todo/DataLoader.java
@@ -23,7 +23,7 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws  Exception {
-//
+
 //        User user = new User("q@q.com","$2a$10$35zYo4fHJ/cjP3QPfMEyDeLSaClFeg1mT1C9q5yMqGl85ccQ/twWO","q@q.com");
 //        userRepository.save(user);
 //        User user2 = new User("a@a.com","$2a$10$35zYo4fHJ/cjP3QPfMEyDeLSaClFeg1mT1C9q5yMqGl85ccQ/twWO","a@a.com");
@@ -31,11 +31,11 @@ public class DataLoader implements ApplicationRunner {
 
         ZadanieEntity zadanieEntity = new ZadanieEntity("jzadanie 1", new Date() , new Date(), new Boolean(true),new User(1L));
         zadanieRepository.save(zadanieEntity);
-        ZadanieEntity zadanieEntity2 = new ZadanieEntity("ToDo cos tam",new Date(), null, new Boolean(false),new User(2L) );
+        ZadanieEntity zadanieEntity2 = new ZadanieEntity("ToDo cos tam",new Date(), null, new Boolean(false),new User(11L) );
         zadanieRepository.save(zadanieEntity2);
         ZadanieEntity zadanieEntity3 = new ZadanieEntity("Lalala",new Date(), null , new Boolean(false),new User(1L) );
         zadanieRepository.save(zadanieEntity3);
-        ZadanieEntity zadanieEntity4 = new ZadanieEntity("Nowe Zadanie",new Date(), null , new Boolean(false),new User(2L) );
+        ZadanieEntity zadanieEntity4 = new ZadanieEntity("Nowe Zadanie",new Date(), null , new Boolean(false),new User(11L) );
         zadanieRepository.save(zadanieEntity4);
 
     }
--- src/main/java/pl/sda/javatarr6/todo/SecuringWebApplicationn.java
+++ src/main/java/pl/sda/javatarr6/todo/SecuringWebApplicationn.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SecuringWebApplicationn {
 
-	//public User zalogowanyUser;
-
-	public static void main(String[] args) throws Throwable {
+		public static void main(String[] args) throws Throwable {
 		SpringApplication.run(SecuringWebApplicationn.class, args);
-		System.out.println("sout z main");
+		//System.out.println("sout z main");
 	}
 
 }
